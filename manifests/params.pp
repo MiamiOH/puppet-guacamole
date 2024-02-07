7101eb26301a45eece96fbc655f2fc0281747ad4
Frank Luo <luoy@miamioh.edu>
2024-02-07 12:36:04 -0500

    fix default listening ip

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,7 @@
 #
 class guacamole::params {
   $server_version    = '1.5.4',
-  $guacd_listen_ip   = $::ip,
+  $guacd_listen_ip   = '127.0.0.1',
   $guacd_listen_port = '4822',
   $install_tomcat    = true,
 }
